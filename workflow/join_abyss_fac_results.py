cbcbf56fdc9ab083596e885fc78532edb25d77a0
Emma Wade <69518571+emmaewade@users.noreply.github.com>
2023-06-25 17:15:45 -0500

    Add files via upload

---
+++ workflow/join_abyss_fac_results.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pandas as pd
+import argparse
+
+parser = argparse.ArgumentParser("")
+parser.add_argument('--master', type=str) 
+parser.add_argument('--abyss_out', type=str) 
+parser.add_argument('--result',  type=str) 
+args = parser.parse_args()
+
+#big table
+joined = pd.read_csv(args.master ,dtype='unicode', sep="\t")
+joined.columns = ["assembly_accession_base", "assembly_accession", "asm_name", "taxid", "organism_name", "infraspecific_name", "version_status", "assembly_level", "ftp_path"]
+#abyss-fac table
+abyss = pd.read_table(args.abyss_out)
+abyss['name_cut'] = abyss['name'].str.slice(2, 17)
+abyss = abyss[~abyss['name'].str.contains('name')]
+abyss_join = pd.merge(joined, abyss, left_on = 'assembly_accession', right_on='name_cut', how = 'outer')
+abyss_join.drop(columns=['name_cut'],inplace=True)
+abyss_join.to_csv(args.result)
+
+    
+#python3 {script} --master {input.master} --abyss_out {input.abyss_out} --result {output.master_abyss}
