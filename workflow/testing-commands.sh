b8defee1d95ad3d2d3fc6c903e5ebc0c96b625f7
Emma Wade <69518571+emmaewade@users.noreply.github.com>
2023-06-25 19:42:32 -0500

    Update testing-commands.sh

--- workflow/testing-commands.sh
+++ workflow/testing-commands.sh
@@ -1,17 +1,17 @@
 snakemake --profile config/  \
     --config \
-        outname=all_omni_419015 \
-        accortaxon=taxonomy \
-        filename=testing-files/taxonomy.txt \
+        outname=acc_test\
+        accortaxon=accession \
+        filename=testing-files/accessions.txt \
         only_download_complete_genomes=f \
-        only_download_latest=t \
+        only_download_latest=f \
         roary_command='-r -p 30 -e --mafft -i 80 -cd 80 -f'
 
 snakemake --profile config/  \
     --config \
-        outname=acc_of_interest \
-        accortaxon=accession \
-        filename=testing-files/taxonomy.txt \
+        outname=taxonomy_test \
+        accortaxon=taxonomy \
+        filename=workflow/testing-files/taxonomy.txt \
         only_download_complete_genomes=f \
-        only_download_latest=f \
+        only_download_latest=t \
         roary_command='-r -p 30 -e --mafft -i 80 -cd 80 -f'
